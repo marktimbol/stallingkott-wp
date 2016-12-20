da18777f0ade2d0f65a12cf8808c314ad0cc2b8f
Mark Timbol <mark.timbol@hotmail.com>
2016-12-20 13:03:54 +0400

    Add env variables

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'stallingkott_wp');
+define('DB_NAME', getenv('DB_NAME'));
 
 /** MySQL database username */
-define('DB_USER', 'stallingkott');
+define('DB_USER', getenv('DB_USER'));
 
 /** MySQL database password */
-define('DB_PASSWORD', 'ARK@Solutions13');
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
